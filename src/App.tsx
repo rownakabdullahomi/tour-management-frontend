9ff9ceda46ea99bfc20f6ac40ca8c48c236afbe0
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-08-05 00:31:01 +0600

    frontend crojectcreated with shadcn

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Simple react app with shadcn</h1>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
