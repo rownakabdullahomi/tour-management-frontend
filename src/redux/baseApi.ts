5139747acd10023277bd8e7d1b81b24d05b14120
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-08-10 00:09:15 +0600

    set tokens in cookie doen

--- src/redux/baseApi.ts
+++ src/redux/baseApi.ts
@@ -1,7 +1,16 @@
-import { createApi } from '@reduxjs/toolkit/query/react'
-import axiosBaseQuery from './axiosBaseQuery'
+import { createApi } from "@reduxjs/toolkit/query/react";
+import axiosBaseQuery from "./axiosBaseQuery";
+
+
+
 export const baseApi = createApi({
-    reducerPath: "baseApi",
-    baseQuery: axiosBaseQuery(),
-    endpoints: ()=>({})
-})
+  reducerPath: "baseApi",
+  baseQuery: axiosBaseQuery(),
+
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: config.baseUrl,
+//     credentials: "include",
+//   }),
+
+  endpoints: () => ({}),
+});
