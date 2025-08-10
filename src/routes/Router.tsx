bee29f6de8ea50a6ef079b604187422127fc977f
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-08-11 01:03:43 +0600

    working with dashboard

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,24 +1,58 @@
 import App from "@/App";
+import DashboardLayout from "@/components/layouts/DashboardLayout";
 import About from "@/pages/About";
+import AddTour from "@/pages/admin/AddTour";
+import Analytics from "@/pages/admin/Analytics";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
+import Bookings from "@/pages/user/Bookings";
 import Verify from "@/pages/Verify";
 
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
-    {
+  {
+    Component: App,
+    path: "/",
+    children: [
+      {
+        Component: About,
+        path: "about",
+      },
+    ],
+  },
 
-        Component: App,
-        path: "/",
-        children: [
-            {
-                Component: About,
-                path: "about",
-            }
-        ]
-    },
-    {
+  {
+    Component: DashboardLayout,
+    path: "/admin",
+    children: [
+      {
+        Component: Analytics,
+        path: "analytics",
+      },
+      {
+        Component: AddTour,
+        path: "add-tour",
+      },
+      {
+        Component: AddTour,
+        path: "add-tour-type",
+      },
+    ],
+  },
+
+  {
+    Component: DashboardLayout,
+    path: "/user",
+    children: [
+      {
+        Component: Bookings,
+        path: "bookings",
+      },
+    ],
+  },
+
+  {
     Component: Login,
     path: "/login",
   },
@@ -30,4 +64,4 @@ export const router = createBrowserRouter([
     Component: Verify,
     path: "/verify",
   },
-])
+]);
